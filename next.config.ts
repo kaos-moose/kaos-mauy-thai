e192599cd58b75c0e2fde7b7e55ca3f9d64a40c1
Criss Moosman <cmoose@Crisss-MacBook-Pro.local>
2026-05-02 14:21:53 -0600

    Reverted config changes

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,6 @@ const nextConfig: NextConfig = {
     RESEND_API_KEY: process.env.RESEND_API_KEY ?? "",
     AWS_REGION: process.env.AWS_REGION ?? "",
     ADMIN_PASSWORD: process.env.ADMIN_PASSWORD ?? "",
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID ?? "",
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY ?? "",
   },
 };
 
